c9cc3d44b251968629db4cd05169c029e3cc2c09
Thomas M Steenholdt <tmus@tmus.dk>
2021-10-20 12:20:20 -0200

    bump local version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 from setuptools import setup
 from setuptools import find_packages
 
-version = "0.2.1"
+version = "0.2.1+thms.3"
 
 with open('README.md') as f:
     long_description = f.read()
